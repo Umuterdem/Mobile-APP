a3ad7099966ece08177b52885846fae09c7c93a5
Umuterdem <61750902+Umuterdem@users.noreply.github.com>
2022-10-08 16:18:30 +0300

    update body.dart and  create  backgorund.dart

--- lib/Screens/Welcome/componets/body.dart
+++ lib/Screens/Welcome/componets/body.dart
@@ -1,37 +1,21 @@
 import 'package:flutter/material.dart';
 
+import 'background.dart';
+
 class Body extends StatelessWidget {
   const Body({super.key});
 
   @override
   Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context)
-        .size; //this  Size provide us total height an Width of our screens
-    return Container(
-      height: size.height,
-      width: double.infinity,
-      child: Stack(
-        alignment: Alignment.center,
+    //this  Size provide us total height an Width of our screens
+    return Background(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-          Positioned(
-              top: 0,
-              left: 0,
-              child: Image.asset(
-                "assets/images/main_top.png",
-                width: size.width * 0.3,
-              )),
-          Positioned(
-              bottom: 0,
-              left: 0,
-              child: Image.asset(
-                "assets/images/main_bottom.png",
-                width: size.width * 0.3,
-              )),
-          Positioned(
-              right: 0,
-              top: 0,
-              child: Image.asset("assets/images/main_top.png",
-                  width: size.width * 0.3))
+          Text(
+            "TKM AKADEMİYE HOŞGELDİNİZ",
+            style: TextStyle(fontWeight: FontWeight.bold),
+          )
         ],
       ),
     );
